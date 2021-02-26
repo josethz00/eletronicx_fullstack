f4ed3f2c8ca76ddeb8c2b46175ef039f04d7c68e
josethz00 <jtsoares17@hotmail.com>
2021-02-26 15:55:48 -0300

    feat: users folder containing the user blueprint and sqlalchemy model

---
+++ server/src/modules/users/user.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from dataclasses import dataclass
+
+from src.shared.database.db import db
+
+
+@dataclass
+class User(db.Model):
+
+    id: int
+    username: str
+    email: str
+    password: str
+    role: str
+    created_at: str
+    updated_at: str
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    role = db.Column(db.String, nullable=False)
+    created_at = db.Column(
+        db.DateTime(),
+        default=datetime.utcnow(),
+        nullable=False
+    )
+    updated_at = db.Column(
+        db.DateTime(),
+        default=datetime.utcnow(),
+        onupdate=datetime.utcnow(),
+        nullable=False
+    )
+
+    def __repr__(self):
+        return '<User %r>' % self.id
---
+++ server/src/modules/users/user_blueprint.py
@@ -0,0 +1,100 @@
+from flask import Blueprint, request, jsonify
+import json
+import hashlib
+from werkzeug.exceptions import Unauthorized
+from threading import Thread
+
+from .user import User
+from src.shared.database.db import db
+from src.shared.utils.jwt_handler import JWTHandler
+from src.shared.utils.mail_handler import send_email
+from src.shared.middlewares.auth_middleware import auth_middleware
+from src.shared.middlewares.role_middleware import role_middleware
+
+user_bp = Blueprint('users', __name__, url_prefix='/users')
+
+
+@user_bp.route('', methods=['GET'])
+@auth_middleware
+@role_middleware
+def index() -> json:
+    users = User.query.all()
+    return jsonify(users), 200
+
+
+@user_bp.route('', methods=['POST'])
+def store() -> json:
+    (
+        username,
+        email,
+        role,
+        password,
+        password_confirmation
+    ) = request.json.values()
+
+    if password != password_confirmation:
+        raise Unauthorized('Password and confirmation do not match')
+
+    hashed_password = hashlib.blake2b(bytes(password, 'utf-8')).hexdigest()
+    new_user = User(
+        username=username,
+        email=email,
+        password=hashed_password,
+        role=role
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+    send_email(
+        new_user.email,
+        f'Welcome {new_user.username}',
+        'We are glad to have you here with us, we hope you enjoy our platform'
+    )
+
+    return json.dumps({'success': f'Welcome, {new_user.username}!'}), 201
+
+
+@user_bp.route('/<int:id>', methods=['PUT'])
+def update(id: int) -> json:
+    username = request.json['username']
+
+    user = User.query.get_or_404(id)
+    user.name = username
+    db.session.commit()
+
+    return jsonify(user), 200
+
+
+@user_bp.route('/<int:id>', methods=['DELETE'])
+def delete(id: int) -> json:
+    user_to_delete = User.query.get_or_404(id)
+
+    db.session.delete(user_to_delete)
+    db.session.commit()
+    return json.dumps({'succes': 'User deleted'}), 200
+
+
+@user_bp.route('/auth', methods=['POST'])
+def authenticate() -> json:
+    if not 'email' not in request.json or 'password' not in request.json:
+        raise Unauthorized('Invalid credentials')
+
+    email, password = request.json.values()
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        raise Unauthorized('Invalid credentials')
+
+    if user.password != hashlib.blake2b(bytes(password, 'utf-8')).hexdigest():
+        raise Unauthorized('Invalid credentials')
+
+    jwt_handler = JWTHandler(user.id)
+
+    return json.dumps(
+        {
+            'id': user.id,
+            'username': user.username,
+            'token': jwt_handler.sign()
+        }
+    ), 201
